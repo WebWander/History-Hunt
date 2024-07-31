db365e6f11db9d380f8ea263ac9c27694006d641
May Marie <lalanganph@gmail.com>
2024-07-31 04:33:44 +0200

    removing trial folder

--- App.js
+++ App.js
@@ -4,8 +4,7 @@ import HomeScreen from './components/screens/HomeScreen';
 import SignUpScreen from "./components/screens/SignUpScreen";
 import LogInScreen from './components/screens/LogInScreen';
 import InviteFriends from './components/invite/InviteFriends';
-import LogIn from './components/trial/LogIn';
-import SignUp from './components/trial/SignUp';
+
 
 const Stack = createNativeStackNavigator();
 
@@ -17,8 +16,6 @@ export default function App() {
         <Stack.Screen options = {{headerShown: false}} name="Login" component={LogInScreen} />
         <Stack.Screen options = {{headerShown: false}} name="Signup" component={SignUpScreen} />
         <Stack.Screen name="HomeScreen" component={HomeScreen} />
-        <Stack.Screen options = {{headerShown: false}} name="LogIn" component={LogIn} /> 
-        <Stack.Screen options = {{headerShown: false}} name="SignUp" component={SignUp} />
         <Stack.Screen options = {{headerShown: false}} name="Invite" component={InviteFriends} /> 
         
         
