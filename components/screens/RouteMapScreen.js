043825c601ebda4d194efd3884f008e14252c17b
NnatasaK <natasakolaric2@gmail.com>
2024-08-17 17:01:12 +0200

    directions API added, transportation mode options, selected marker state change

--- components/screens/RouteMapScreen.js
+++ components/screens/RouteMapScreen.js
@@ -1,15 +1,22 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity, Modal, Alert } from 'react-native';
+import { View, Text, TouchableOpacity, Modal, Alert, Pressable, Image } from 'react-native';
 import MapView, { Marker, Polyline } from 'react-native-maps';
-import { Ionicons } from '@expo/vector-icons';
-import { SafeAreaView } from 'react-native-safe-area-context';
 import PropTypes from 'prop-types';
 import * as Location from 'expo-location';
+import { LinearGradient } from 'expo-linear-gradient';
+import { useAuth } from '../../context/authContext';
+import HistoryHuntImage from '../../assets/letter.jpg';
+import axios from 'axios';
+import { Ionicons } from '@expo/vector-icons';
 
-const RouteMapScreen = ({ route, navigation }) => {
+const RouteMapScreen = ({ route }) => {
   const { huntData } = route.params || { huntData: { title: 'Default Title', markers: [] } };
   const [location, setLocation] = useState(null);
+  const [routeCoordinates, setRouteCoordinates] = useState([]);
+  const [transportationMode, setTransportationMode] = useState('driving'); 
+  const [selectedMarker, setSelectedMarker] = useState(null);
   const [modalVisible, setModalVisible] = useState(true);
+  const { user } = useAuth();
 
   useEffect(() => {
     (async () => {
@@ -24,14 +31,107 @@ const RouteMapScreen = ({ route, navigation }) => {
     })();
   }, []);
 
+  useEffect(() => {
+    if (location && selectedMarker) {
+      getDirections(location, selectedMarker.coordinate, transportationMode);
+    }
+  }, [transportationMode, selectedMarker, location]);
+
+  const decodePolyline = (encoded) => {
+    let points = [];
+    let index = 0, len = encoded.length;
+    let lat = 0, lng = 0;
+
+    while (index < len) {
+      let b, shift = 0, result = 0;
+      do {
+        b = encoded.charCodeAt(index++) - 63;
+        result |= (b & 0x1f) << shift;
+        shift += 5;
+      } while (b >= 0x20);
+      let dlat = ((result & 1) ? ~(result >> 1) : (result >> 1));
+      lat += dlat;
+
+      shift = 0;
+      result = 0;
+      do {
+        b = encoded.charCodeAt(index++) - 63;
+        result |= (b & 0x1f) << shift;
+        shift += 5;
+      } while (b >= 0x20);
+      let dlng = ((result & 1) ? ~(result >> 1) : (result >> 1));
+      lng += dlng;
+
+      points.push({
+        latitude: lat / 1e5,
+        longitude: lng / 1e5
+      });
+    }
+
+    return points;
+  };
+
+  const getDirections = async (startLoc, destinationLoc, mode = 'driving') => {
+    try {
+      const apiKey = 'AIzaSyA4rsU2CLdCSerkMXufguIp2B1YadQRrvo';
+      const url = `https://maps.googleapis.com/maps/api/directions/json?origin=${startLoc.latitude},${startLoc.longitude}&destination=${destinationLoc.latitude},${destinationLoc.longitude}&mode=${mode}&key=${apiKey}`;
+      const response = await axios.get(url);
+  
+      if (response.data.routes.length) {
+        const route = response.data.routes[0].overview_polyline.points;
+        const decodedPoints = decodePolyline(route);
+        setRouteCoordinates(decodedPoints);
+      }
+    } catch (error) {
+      console.error('Error getting directions:', error);
+    }
+  };
+  
   const getMarkerColor = (index) => {
     if (index === 0) return '#0951E2'; // Blue for the first marker
     if (index === huntData.markers.length - 1) return '#B73FFC'; // Purple for the last marker
     return 'orange'; // All other markers
   };
 
+  const handleMarkerPress = (marker) => {
+    if (location) {
+      /* getDirections(location, marker.coordinate, transportationMode); */
+      setSelectedMarker(marker);
+    }
+  };
+  
+
   return (
-    <SafeAreaView className="flex-1">
+    <View className="flex-1">
+      <LinearGradient
+        colors={['#BE3CFB', '#0951E2']}
+        start={[0, 0]}
+        end={[0, 1]}
+        locations={[0, 0.7]}
+        className="h-50 justify-center"
+      >
+        
+        <Text className="text-center text-2xl font-extrabold mt-20" style={{ color: "#FF9800" }}>NEXT CLUE</Text>
+        <Text className="text-center pt-1 text-white" style={{ fontSize: 16 }}>Make your way to</Text>
+        <Text className="text-center pt-1 text-xl font-bold text-white">{selectedMarker?.title || "Select a location"}</Text>
+
+        <View className="flex-row justify-around p-4 mt-4">
+          <TouchableOpacity onPress={() => setTransportationMode('driving')}>
+            <Ionicons name="car" size={32} color={transportationMode === 'driving' ? '#FF9800' : 'white'} />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => setTransportationMode('walking')}>
+            <Ionicons name="walk" size={32} color={transportationMode === 'walking' ? '#FF9800' : 'white'} />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => setTransportationMode('bicycling')}>
+            <Ionicons name="bicycle" size={32} color={transportationMode === 'bicycling' ? '#FF9800' : 'white'} />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => setTransportationMode('transit')}>
+            <Ionicons name="bus" size={32} color={transportationMode === 'transit' ? '#FF9800' : 'white'} />
+          </TouchableOpacity>
+        </View>
+
+      </LinearGradient>
+
       <MapView
         style={{ flex: 1 }}
         initialRegion={{
@@ -43,46 +143,62 @@ const RouteMapScreen = ({ route, navigation }) => {
         showsUserLocation={true}
       >
         {huntData.markers.map((marker, index) => (
-          <Marker key={index} coordinate={marker.coordinate} title={index === 0 || index === huntData.markers.length - 1 ? `Spot ${index + 1}` : null}>
+          <Marker
+            key={index}
+            coordinate={marker.coordinate}
+            title={marker.title || `Spot ${index + 1}`}
+            onPress={() => handleMarkerPress(marker)}
+          >
             <View style={{ backgroundColor: getMarkerColor(index), paddingVertical: 2, paddingHorizontal: 6, borderRadius: 10 }}>
               <Text style={{ color: 'white', fontWeight: 'bold' }}>{index + 1}</Text>
             </View>
           </Marker>
         ))}
-        {huntData.markers.length > 1 && (
+        {routeCoordinates.length > 0 && (
           <Polyline
-            coordinates={huntData.markers.map(marker => marker.coordinate)}
-            strokeColor="#FFA500"
-            strokeWidth={4}
+          coordinates={routeCoordinates/* .length > 0 ? routeCoordinates : huntData.markers.map(marker => marker.coordinate) */}
+          strokeColor="orange" 
+          strokeWidth={4}
           />
         )}
       </MapView>
 
-      {/* Pop-up modal */}
       <Modal
-        animationType="slide"
+        animationType="fade"
         transparent={true}
         visible={modalVisible}
-        onRequestClose={() => {
-          setModalVisible(!modalVisible);
-        }}
+        onRequestClose={() => setModalVisible(false)}
       >
-        <View className="flex-1 justify-center items-center">
-          <View className="bg-black-200 rounded-lg p-6 mx-10 shadow-lg">
-            <Text className="text-lg text-center font-bold mb-4">Next Clue</Text>
-            <Text className="text-center mb-6">Make your way to {huntData.markers[0]?.title || "the next location"}.</Text>
-            <Text className="text-center mb-6">After you begin a hunt, you can choose your transportation method to travel to the next clue. Tap anywhere to continue.</Text>
-            <TouchableOpacity onPress={() => setModalVisible(!modalVisible)} className="absolute top-2 right-2">
-              <Ionicons name="close" size={24} color="black" />
-            </TouchableOpacity>
+        <Pressable style={{ flex: 1 }} onPress={() => setModalVisible(false)}>
+          <View className="flex-1 justify-center bg-black/50">
+            <View className="rounded-3xl p-4 mb-60 mx-12 shadow-lg" style={{ backgroundColor: '#282D41' }}>
+              <Text className="text-center mt-2" style={{ fontSize: 18, color: 'rgba(238, 238, 238, 0.8)' }}>After you begin a hunt, you can choose your transportation method to travel to the next clue.</Text>
+              <Text className="text-center font-bold mb-2 mt-2" style={{ fontSize: 18, color: 'rgba(238, 238, 238, 0.8)' }}>Tap anywhere to continue.</Text>
+            </View>
           </View>
-        </View>
+        </Pressable>
       </Modal>
 
-      <TouchableOpacity className="absolute top-12 left-4" onPress={() => navigation.goBack()}>
-        <Ionicons name="arrow-back" size={32} color="#0951E2" />
+      <View className="absolute bottom-16 left-4">
+        <Image
+          source={{ uri: user?.profileImageUrl || 'https://example.com/default-avatar.jpg' }}
+          className="w-16 h-16 rounded-full border-2 border-white"
+        />
+      </View>
+
+      <TouchableOpacity
+        className="absolute bottom-14 right-36"
+        onPress={console.log('next step')}
+      >
+        <View className="flex-column items-center px-4 py-2">
+          <Image
+            source={HistoryHuntImage}
+            className="w-16 h-16 rounded-full border-0.5"
+            style={{ borderColor: '#ba55d3', color: '#ba55d3' }}
+          />
+        </View>
       </TouchableOpacity>
-    </SafeAreaView>
+    </View>
   );
 };
 
@@ -97,6 +213,7 @@ RouteMapScreen.propTypes = {
               latitude: PropTypes.number.isRequired,
               longitude: PropTypes.number.isRequired,
             }).isRequired,
+            title: PropTypes.string, // Ensure the title prop is accounted for
           })
         ).isRequired,
       }).isRequired,
